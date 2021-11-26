d3e04b8c1fcfae19000473e33ae88aec3bc73cf4
Bruno Rodrigues <bruno@agenciaeplus.com.br>
2021-11-25 21:06:57 -0300

    Initial commit 🎉

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from '../styles/pages/Home'
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Teste boilerplate</h1>
+    </Container>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    background: '#ffffff',
+    text: '#323730',
+
+    green: '#41b932'
+  }
+}
+
+export default theme
